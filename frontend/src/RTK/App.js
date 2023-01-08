019f416c48a58f465cab471fa620df0bce2ebb03
SHARUN266 <sharunattari266@gmail.com>
2023-01-08 13:20:56 +0530

    Backend

--- frontend/src/RTK/App.js
+++ frontend/src/RTK/App.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Returns a user s api.
 export const UserApi = createApi({
   reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4500/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://cointabbackend.onrender.com/" }),
   // Get all users.
   endpoints: (builder) => ({
     allUser: builder.query({
